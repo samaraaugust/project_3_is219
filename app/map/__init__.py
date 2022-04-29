538152d8f5e9e8371217c5a1bd8033786630c66e
samaraaugust <samaraaugust123@gmail.com>
2022-04-29 17:33:06 -0400

    added html and method for adding new locations

--- app/map/__init__.py
+++ app/map/__init__.py
@@ -1,6 +1,6 @@
 from flask_login import login_required, current_user
 from flask import Blueprint, render_template, abort, flash, current_app, url_for
-from app.map.forms import csv_upload, edit_location
+from app.map.forms import csv_upload, edit_location, new_location
 from jinja2 import TemplateNotFound
 from app.db.models import Location
 from app.db import db
@@ -36,6 +36,19 @@ def edit_locations(location_id):
         return redirect(url_for('map.browse_locations'))
     return render_template('location_edit.html', form=form)
 
+@map.route('/locations/new', methods=['POST', 'GET'])
+@login_required
+def add_location():
+    form = new_location()
+    if form.validate_on_submit():
+        location = Location(title=form.title.data, longitude=form.longitude.data, latitude=form.latitude.data, population=form.population.data)
+        db.session.add(location)
+        db.session.commit()
+        flash('Congratulations, you just add a new location', 'success')
+        return redirect(url_for('map.browse_locations'))
+
+    return render_template('new_location.html', form=form)
+
 @map.route('/locations/map/<int:location_id>')
 @login_required
 def retrieve_location(location_id):
@@ -52,8 +65,9 @@ def browse_locations(page):
     edit_url = ('map.edit_locations', [('location_id', ':id')])
     retrieve_url = ('map.retrieve_location', [('location_id', ':id')])
     delete_url = ('map.delete_location', [('location_id', ':id')])
+    add_url = url_for('map.add_location')
     try:
-        return render_template('browse_locations.html',retrieve_url=retrieve_url, edit_url=edit_url,Location=Location, data=data,pagination=pagination, delete_url=delete_url)
+        return render_template('browse_locations.html',retrieve_url=retrieve_url, edit_url=edit_url, add_url=add_url, Location=Location, data=data,pagination=pagination, delete_url=delete_url)
     except TemplateNotFound:
         abort(404)
 
--- app/map/forms/__init__.py
+++ app/map/forms/__init__.py
@@ -12,3 +12,10 @@ class edit_location(FlaskForm):
     latitude = TextAreaField('Latitude', [validators.length(min=1, max=300)])
     population = TextAreaField('Population', [validators.length(min=1, max=300)])
     submit = SubmitField()
+
+class new_location(FlaskForm):
+    title = TextAreaField('Title', [validators.length(min=1, max=300)])
+    longitude = TextAreaField('Longitude', [validators.length(min=1, max=300)])
+    latitude = TextAreaField('Latitude', [validators.length(min=1, max=300)])
+    population = TextAreaField('Population', [validators.length(min=1, max=300)])
+    submit = SubmitField()
