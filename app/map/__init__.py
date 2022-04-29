c1e12db6032a9f7530717e3e1f06259b42420591
samaraaugust <samaraaugust123@gmail.com>
2022-04-29 14:04:02 -0400

    added to uploads method, files get saved to folder

--- app/map/__init__.py
+++ app/map/__init__.py
@@ -1,7 +1,9 @@
 from flask_login import login_required
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, current_app
 from app.map.forms import csv_upload
 from jinja2 import TemplateNotFound
+import os
+from werkzeug.utils import secure_filename
 
 map = Blueprint('map', __name__,
                         template_folder='templates')
@@ -10,6 +12,10 @@ map = Blueprint('map', __name__,
 @login_required
 def upload():
     form = csv_upload()
+    if form.validate_on_submit():
+        filename = secure_filename(form.file.data.filename)
+        filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+        form.file.data.save(filepath)
     try:
         return render_template('upload_map.html', form=form)
     except TemplateNotFound:
