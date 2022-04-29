1d373d6bf72593649a96b17406a03a1541cf983f
samaraaugust <samaraaugust123@gmail.com>
2022-04-29 14:07:30 -0400

    locations get added to db

--- app/map/__init__.py
+++ app/map/__init__.py
@@ -1,7 +1,10 @@
-from flask_login import login_required
+from flask_login import login_required, current_user
 from flask import Blueprint, render_template, abort, current_app
 from app.map.forms import csv_upload
 from jinja2 import TemplateNotFound
+from app.db.models import Location
+from app.db import db
+import csv
 import os
 from werkzeug.utils import secure_filename
 
@@ -16,6 +19,15 @@ def upload():
         filename = secure_filename(form.file.data.filename)
         filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
         form.file.data.save(filepath)
+        list_of_locations = []
+        with open(filepath) as file:
+            csv_file = csv.DictReader(file)
+            for row in csv_file:
+                list_of_locations.append(
+                    Location(row['location'], row['longitude'], row['latitude'], row['population']))
+
+        current_user.locations = list_of_locations
+        db.session.commit()
     try:
         return render_template('upload_map.html', form=form)
     except TemplateNotFound:
