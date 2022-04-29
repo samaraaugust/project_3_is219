5865fa8f3b85bc8f523d9af8fed6129298fae2fd
samaraaugust <samaraaugust123@gmail.com>
2022-04-29 16:54:55 -0400

    added function for editing locations

--- app/map/forms/__init__.py
+++ app/map/forms/__init__.py
@@ -5,3 +5,10 @@ from wtforms.fields import *
 class csv_upload(FlaskForm):
     file = FileField()
     submit = SubmitField()
+
+class edit_location(FlaskForm):
+    title = TextAreaField('Title', [validators.length(min=1, max=300)])
+    longitude = TextAreaField('Longitude', [validators.length(min=1, max=300)])
+    latitude = TextAreaField('Latitude', [validators.length(min=1, max=300)])
+    population = TextAreaField('Population', [validators.length(min=1, max=300)])
+    submit = SubmitField()
